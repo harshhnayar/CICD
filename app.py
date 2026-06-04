4ebc763a0e5c49c2a7b76557dbf4b1989a010fa1
harsh nayyar <harshhnayar125@gmail.com>
2026-06-04 19:40:20 +0530

    added multiplication filter

--- app.py
+++ app.py
@@ -2,9 +2,15 @@
 def add_numbers(a, b):
     return a + b
 
+def multiply_numbers(a, b):
+    return a * b
+
 if __name__ == "__main__":
-    result = add_numbers(5, 7)
-    output_text = f"Hello World! The calculation result is: {result}"
+    calc_sum = add_numbers(5, 7)
+    calc_prod = multiply_numbers(4, 5)
+    
+    output_text = f"Calculation Results -> Added: {calc_sum} | Multiplied: {calc_prod}"
     print(output_text)
+    
     with open("result.txt", "w") as f:
         f.write(output_text + "\n")
